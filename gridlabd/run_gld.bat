36e97f14d111b193084cdfaaa5370b62fe02846f
Jing Xie <xjasura@gmail.com>
2019-02-22 08:38:02 -0800

    GLD: Check if the folder path exists before adding it to PATH

--- gridlabd/run_gld.bat
+++ gridlabd/run_gld.bat
@@ -1,3 +1,5 @@
+@echo off
+
 :: Config Params
 set gld_bin_path=D:\GridLAB-D\bin
 set mgw_bin_path=C:\msys32\mingw64\bin
@@ -6,7 +8,20 @@ set gld_lib_glpath=D:\GridLAB-D\lib\gridlabd
 set gld_share_glpath=D:\GridLAB-D\share\gridlabd
 
 :: Start [Note that setx does changes permanently]
-SET PATH=%PATH%;%gld_bin_path%;%mgw_bin_path%
+if exist %gld_bin_path% (
+	SET PATH=%PATH%;%gld_bin_path%;
+) else (
+	echo PATH %gld_bin_path% does not exist!
+	pause
+)
+
+if exist %mgw_bin_path% (
+	SET PATH=%PATH%;%mgw_bin_path%;
+) else (
+	echo PATH %mgw_bin_path% does not exist!
+	pause
+)
+
 SET GLPATH=%gld_lib_glpath%;%gld_share_glpath%
 
 :: Run and Open the CMD Window
